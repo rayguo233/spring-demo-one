d4f4deb10a98b8c2570212b060ede908528e8020
Ray Guo <siruiguo@outlook.com>
2021-05-10 18:30:13 +0800

    implement injecting literal values

--- com/luv2code/springdemo/CricketCoach.java
+++ com/luv2code/springdemo/CricketCoach.java
@@ -3,12 +3,31 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	private String emailAddress;
+	private String team;
 	
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
 	}
 	
-	// our setter method
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
 	public void setFortuneService(FortuneService fortuneService) {
 		System.out.println("CricketCoach: inside setter method - setFortuneService");
 		this.fortuneService = fortuneService;
