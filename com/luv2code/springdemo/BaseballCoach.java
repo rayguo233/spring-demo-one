67bc68eb23ebb9819f087b15a155746670e16382
Ray Guo <siruiguo@outlook.com>
2021-05-10 16:10:25 +0800

    implement dependency injection

--- com/luv2code/springdemo/BaseballCoach.java
+++ com/luv2code/springdemo/BaseballCoach.java
@@ -2,8 +2,19 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 
-		@Override
-		public String getDailyWorkout() {
-			return "Spend 30 minutes on batting practice";
-		}
+	private FortuneService fortuneService;
+	
+	public BaseballCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
+	
+	@Override
+	public String getDailyWorkout() {
+		return "Spend 30 minutes on batting practice";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
 }
--- com/luv2code/springdemo/MyApp.java
+++ com/luv2code/springdemo/MyApp.java
@@ -3,10 +3,8 @@ package com.luv2code.springdemo;
 public class MyApp {
 
 	public static void main(String[] args) {
-		// create the object
-		Coach theCoach = new TrackCoach();
-		// use the object
-		System.out.println(theCoach.getDailyWorkout());
+//		Coach theCoach = new TrackCoach();
+//		System.out.println(theCoach.getDailyWorkout());
 	}
 
 }
