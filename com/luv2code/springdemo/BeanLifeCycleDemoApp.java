b07229f4dcbdb3310481eeff64c5d142ea326a7f
Ray Guo <siruiguo@outlook.com>
2021-05-10 22:04:06 +0800

    play with bean life cycle

---
+++ com/luv2code/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,19 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		Coach alphaCoach = context.getBean("myCoach", Coach.class);
+		boolean result = theCoach == alphaCoach;
+		System.out.println("Pointing to the same object: " + result);
+		System.out.println("Memory location for theCoach: " + theCoach);
+		System.out.println("Memory location for alphaCoach: " + alphaCoach);
+		context.close();
+	}
+
+}
--- com/luv2code/springdemo/BeanScopeDemoApp.java
+++ com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -6,13 +6,9 @@ public class BeanScopeDemoApp {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+				new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
 		Coach theCoach = context.getBean("myCoach", Coach.class);
-		Coach alphaCoach = context.getBean("myCoach", Coach.class);
-		boolean result = theCoach == alphaCoach;
-		System.out.println("Pointing to the same object: " + result);
-		System.out.println("Memory location for theCoach: " + theCoach);
-		System.out.println("Memory location for alphaCoach: " + alphaCoach);
+		System.out.println(theCoach.getDailyWorkout());
 		context.close();
 	}
 
